a19e31ac2cc4bae3ffda2c47a779e3207fe97418
MrDzhofik <djafarovemil04@mail.ru>
2025-04-02 19:46:28 +0300

    Add test and prettier view

--- 02/process.py
+++ 02/process.py
@@ -1,21 +1,26 @@
 import json
 from typing import Callable
 
-def process_json(json_str: str, keys: list[str] | None = None, tokens: list[str] | None = None, callback: Callable[[str, str], None] | None = None) -> None:
+
+def process_json(
+        json_str: str,
+        keys: list[str] | None = None,
+        tokens: list[str] | None = None,
+        callback: Callable[[str, str], None] | None = None
+        ) -> None:
     dct = json.loads(json_str)
     if keys is None:
         keys = dct.keys()
-
     if tokens is None:
         return
-    
-    tokens = {token.lower() for token in tokens} 
+
+    tokens_lower = {token.lower() for token in tokens}
 
     for key in keys:
         if key in dct:
             value = dct[key].split()
             for word in value:
-                if word.lower() in tokens:
+                if word.lower() in tokens_lower:
                     if callback:
                         callback(key, word)
                     else:
@@ -24,10 +29,3 @@ def process_json(json_str: str, keys: list[str] | None = None, tokens: list[str]
 
 def print_result(key: str, token: str) -> None:
     print(f"key={key}, token={token}")
-
-
-if __name__ == "__main__":
-    json_str = '{"key1": "Word1 word2", "key2": "word2 word3"}'
-    required_keys = ["key1", "KEY2"]
-    tokens = ["WORD1", "word2"]
-    process_json(json_str=json_str, keys=required_keys, tokens=tokens, callback=print_result)
--- 02/test_proccess.py
+++ 02/test_proccess.py
@@ -1,39 +1,93 @@
+from pytest import CaptureFixture
 from process import process_json, print_result
-import sys
-import os
 
 
-def test_success(capsys):
-    json_str = '{"key1": "Word1 word2", "key2": "word2 word3"}'
+def test_success(capsys: CaptureFixture[str]):
+    json_str = """{
+    "key1": "Word1 word2", 
+    "key2": "word2 word3"
+    }"""
     required_keys = ["key1", "KEY2"]
     tokens = ["WORD1", "word2"]
     process_json(json_str, required_keys, tokens)
 
-    captured = capsys.readouterr() 
+    captured = capsys.readouterr()
     expected_output = "key=key1, token=Word1\nkey=key1, token=word2\n"
-    
+
     assert captured.out == expected_output
 
 
-def test_with_calback(capsys):
-    json_str = '{"key1": "Word1 word2", "key2": "word2 word3"}'
+def test_with_calback(capsys: CaptureFixture[str]):
+    json_str = """{
+    "key1": "Word1 word2", 
+    "key2": "word2 word3"
+    }"""
     required_keys = ["key1", "KEY2"]
     tokens = ["WORD1", "word2"]
-    process_json(json_str, required_keys, tokens)
+    process_json(json_str, required_keys, tokens, print_result)
 
-    captured = capsys.readouterr() 
+    captured = capsys.readouterr()
     expected_output = "key=key1, token=Word1\nkey=key1, token=word2\n"
-    
+
     assert captured.out == expected_output
 
 
-def test_no_tokens(capsys):
-    json_str = '{"key1": "Word1 word2", "key2": "word2 word3"}'
+def test_no_tokens(capsys: CaptureFixture[str]):
+    json_str = """{
+    "key1": "Word1 word2", 
+    "key2": "word2 word3"
+    }"""
     required_keys = ["key1", "KEY2"]
-    tokens = ["WORD1", "word2"]
     process_json(json_str, required_keys)
 
-    captured = capsys.readouterr() 
+    captured = capsys.readouterr()
     expected_output = ""
-    
+
+    assert captured.out == expected_output
+
+
+def test_no_keys(capsys: CaptureFixture[str]):
+    json_str = """{
+    "Messi": "Lionel",
+    "Ronaldo": "Cristiano dus Santos Aveiru",
+    "Zlatan": "Ibragimovic"
+    }"""
+    tokens = ["Lionel", "Cristiano"]
+    process_json(json_str=json_str, tokens=tokens)
+
+    captured = capsys.readouterr()
+    expected_output = "key=Messi, token=Lionel\nkey=Ronaldo, token=Cristiano\n"
+
+    assert captured.out == expected_output
+
+
+def test_empty_tokens(capsys: CaptureFixture[str]):
+    json_str = """{
+    "Messi": "Lionel",
+    "Ronaldo": "Cristiano dus Santos Aveiru",
+    "Zlatan": "Ibragimovic"
+    }"""
+    keys = ["Messi", "Ronaldo"]
+    tokens = ["", ""]
+    process_json(json_str=json_str, keys=keys, tokens=tokens)
+
+    captured = capsys.readouterr()
+    expected_output = ""
+
+    assert captured.out == expected_output
+
+
+def test_empty_keys(capsys: CaptureFixture[str]):
+    json_str = """{
+    "Messi": "Lionel",
+    "Ronaldo": "Cristiano dus Santos Aveiru",
+    "Zlatan": "Ibragimovic"
+    }"""
+    keys = ["", ""]
+    tokens = ["Lionel", "Ibragimovic"]
+    process_json(json_str=json_str, keys=keys, tokens=tokens)
+
+    captured = capsys.readouterr()
+    expected_output = ""
+
     assert captured.out == expected_output
